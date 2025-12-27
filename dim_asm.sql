55a07f3e1db5dca93c5909807ca1733a4863140a
hanhptm02 <hanhpham290602@gmail.com>
2025-12-27 16:17:21 +0700

    Add files via upload

--- dim_asm.sql
+++ dim_asm.sql
@@ -1,5 +1,5 @@
-CREATE TABLE dim_asm (
-	id int4 primary key,
+create table dim_asm (
+	id serial primary key,
 	sales_name varchar(1024),
-	sales_email varchar(1024)
+	sales_email varchar(1024) unique
 );
